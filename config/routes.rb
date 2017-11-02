cd8756d7213e10826b40ff4387863118913ed5db
3413 <3413@example.com>
2017-11-02 10:03:09 +0900

    予約

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :yoyakus
-=======
   get 'login/index'
 
   post 'login/auth'
 
   get 'home/kari'
 
->>>>>>> 8759e5f2277f0b1d2b96ec3001f3c1a4d028cf06
   resources :mtourokus
   resources :genres
   resources :users
