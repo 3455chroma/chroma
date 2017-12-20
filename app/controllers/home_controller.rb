7256de1d410ea8cb2e1c026f52399b882ebe7bca
3418 <3418@example.com>
2017-12-20 09:28:06 +0900

    お願い出来て。。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,13 @@ class HomeController < ApplicationController
     @book    = Genre.where(name: "本")
     @cd      = Genre.where(name: "CD")
     @dvd     = Genre.where(name: "DVD")
-    @tsuchi  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today-3,Date.today+3).order(hatsubaiday: :desc)
+    @tsuchi1  = Stouroku.where("hatsubaiday = ? and hatsubaiday <= ?",Date.today-3,Date.today-3).order(hatsubaiday: :desc)
+    @tsuchi2  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today-2,Date.today-2).order(hatsubaiday: :desc)
+    @tsuchi3  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today-1,Date.today-1).order(hatsubaiday: :desc)
+    @tsuchi4  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today-0,Date.today-0).order(hatsubaiday: :desc)
+    @tsuchi5  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today+1,Date.today+1).order(hatsubaiday: :desc)
+    @tsuchi6  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today+2,Date.today+2).order(hatsubaiday: :desc)
+    @tsuchi7  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today+3,Date.today+3).order(hatsubaiday: :desc)
     
   end
   
