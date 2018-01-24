b0e8e934152de6d782ba4922f9fe5079773679d6
3404 <3404@example.com>
2018-01-24 10:27:35 +0900

    集大成３

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,13 @@ class HomeController < ApplicationController
     @book    = Genre.where(name: "本")
     @cd      = Genre.where(name: "CD")
     @dvd     = Genre.where(name: "DVD")
-    @tsuchi1  = Stouroku.where("hatsubaiday = ? and hatsubaiday <= ?",Date.today-3,Date.today-3).order(hatsubaiday: :desc)
-    @tsuchi2  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today-2,Date.today-2).order(hatsubaiday: :desc)
-    @tsuchi3  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today-1,Date.today-1).order(hatsubaiday: :desc)
-    @tsuchi4  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today-0,Date.today-0).order(hatsubaiday: :desc)
-    @tsuchi5  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today+1,Date.today+1).order(hatsubaiday: :desc)
-    @tsuchi6  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today+2,Date.today+2).order(hatsubaiday: :desc)
-    @tsuchi7  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ?",Date.today+3,Date.today+3).order(hatsubaiday: :desc)
+    @tsuchi1  = Stouroku.where("hatsubaiday = ? and hatsubaiday <= ? and user_id = ?",Date.today-3,Date.today-3,@usr).order(hatsubaiday: :desc)
+    @tsuchi2  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ? and user_id = ?",Date.today-2,Date.today-2,@usr).order(hatsubaiday: :desc)
+    @tsuchi3  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ? and user_id = ?",Date.today-1,Date.today-1,@usr).order(hatsubaiday: :desc)
+    @tsuchi4  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ? and user_id = ?",Date.today-0,Date.today-0,@usr).order(hatsubaiday: :desc)
+    @tsuchi5  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ? and user_id = ?",Date.today+1,Date.today+1,@usr).order(hatsubaiday: :desc)
+    @tsuchi6  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ? and user_id = ?",Date.today+2,Date.today+2,@usr).order(hatsubaiday: :desc)
+    @tsuchi7  = Stouroku.where("hatsubaiday >= ? and hatsubaiday <= ? and user_id = ?" ,Date.today+3,Date.today+3,@usr).order(hatsubaiday: :desc)
     
   end
   
